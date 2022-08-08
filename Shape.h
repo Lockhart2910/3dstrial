90545bf2d3584151eee1d74c277962863e84a256
Lockhart2910 <110320893+Lockhart2910@users.noreply.github.com>
2022-08-08 10:47:08 +0530

    Add files via upload

---
+++ Shape.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+class Shape{
+public:
+	virtual double calculateArea()const { return 0.0; }
+	virtual double calculateVolume()const { return 0.0; }
+	virtual void drawshape()const = 0;
+	virtual void print()const = 0;
+};
+
+void virtualViaPointer(const Shape*);
+void virtualViaReference(const Shape& baseClassRef);
